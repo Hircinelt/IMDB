15da178abdaf8957f5ac444a6289592624f96b89
Hircinelt <79124196+Hircinelt@users.noreply.github.com>
2023-08-30 19:09:49 +0300

    Update MSSMS.py

--- MSSMS.py
+++ MSSMS.py
@@ -32,15 +32,23 @@ while True:
             title = movie.select('.lister-item-header a')[0].get_text().strip()
             year = movie.select('.lister-item-year')[0].get_text().strip('()')
             duration = movie.select('.runtime')[0].get_text() if movie.select('.runtime') else ""
-            gross = ""
-            gross_element = movie.find('span', string='Gross:')
-            if gross_element:
-                gross = gross_element.find_next_sibling().get_text().strip()
-            director = movie.select('.text-muted .text-muted')[2].get_text().split('|')[0].strip() if len(movie.select('.text-muted .text-muted')) > 2 else ""
-            stars = movie.select('.text-muted .text-muted')[2].get_text().split('|')[1].strip() if len(movie.select('.text-muted .text-muted')) > 2 else ""
+            gross = movie.find('span', {'name': 'nv'}).get('data-value').replace(',', '') if movie.find('span', {'name': 'nv'}) else ""
+            director = movie.select_one('a[href^="/name/"]').get_text() if movie.select_one('a[href^="/name/"]') else ""
+            stars_elements = movie.select('p:-soup-contains("Stars:") a')
+            stars = ', '.join([star.get_text() for star in stars_elements])
+            star1 = stars_elements[1].get_text() if len(stars_elements) >= 1 else ""
+            second_star_element = stars_elements[2] if len(stars_elements) >= 2 else None
+            star2 = second_star_element.get_text() if second_star_element else ""
+            third_star_element = stars_elements[3] if len(stars_elements) >= 3 else None
+            star3 = third_star_element.get_text() if third_star_element else ""
+            fourth_star_element = stars_elements[4] if len(stars_elements) >= 4 else None
+            star4 = fourth_star_element.get_text() if fourth_star_element else ""
             genre = movie.select('.genre')[0].get_text().strip()
-            
-            
+            genres = movie.select('.genre')[0].get_text().strip().split(', ')
+            genre1 = genres[0] if len(genres) >= 1 else ""
+            genre2 = genres[1] if len(genres) >= 2 else ""
+            genre3 = genres[2] if len(genres) >= 3 else ""
+    
             data = {
                 "title": title,
                 "year": year,
@@ -51,7 +59,14 @@ while True:
                 "genre": genre,
                 "rating": rating,
                 "votes": votes,
-                "metascore": metascore
+                "metascore": metascore,
+                "star1": star1,
+                "star2": star2,
+                "star3": star3,
+                "star4": star4,
+                "genre1": genre1,
+                "genre2": genre2,
+                "genre3": genre3
             }
             movie_data_list.append(data)
         except IndexError:
@@ -65,8 +80,8 @@ while True:
 # Insert data into the database
 for data in movie_data_list:
     insert_query = '''
-    INSERT INTO IMDB (Title, Year, Duration, Gross, Director, Stars, Genre, Rating, Votes, Metascore)
-    VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?)
+    INSERT INTO IMDB (Title, Year, Duration, Gross, Director, Stars, Genre, Rating, Votes, Metascore, Star1, Star2, Star3, Star4, Genre1, Genre2, Genre3)
+    VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)
     '''
     values = (
         data["title"],
@@ -78,7 +93,14 @@ for data in movie_data_list:
         data["genre"],
         data["rating"],
         data["votes"],
-        data["metascore"]
+        data["metascore"],
+        data["star1"],
+        data["star2"],
+        data["star3"],
+        data["star4"],
+        data["genre1"],
+        data["genre2"],
+        data["genre3"]
     )
     cursor.execute(insert_query, values)
     conn.commit()
