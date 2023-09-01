6dee12e9747e5070aeacb01448164742294a11c1
Hircinelt <79124196+Hircinelt@users.noreply.github.com>
2023-09-02 02:13:16 +0300

    Add files via upload

--- Procedure - UpdateColumns.sql
+++ Procedure - UpdateColumns.sql
@@ -1,21 +1,31 @@
-USE [TEST]
+USE [IMDB]
 GO
+
 SET ANSI_NULLS ON
 GO
+
 SET QUOTED_IDENTIFIER ON
 GO
+
+
 
 
 
 ALTER PROCEDURE [dbo].[UpdateColumns]
 AS
 BEGIN
-    UPDATE IMDB
+    UPDATE Movies
     SET Duration = LEFT(Duration, CHARINDEX(' ', Duration) - 1)
     WHERE CHARINDEX(' ', Duration) > 0;
-	UPDATE IMDB
+	UPDATE Movies
     SET Year = SUBSTRING(Year, PATINDEX('%[0-9]%', Year), 4)
     WHERE PATINDEX('%[0-9]%', Year) > 0;
+	DECLARE @Counter INT = 0;
+    UPDATE Movies
+    SET ID = @Counter, @Counter = @Counter + 1;
 END;
+
+
+GO
 
 
